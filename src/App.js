2418f2d1805a0ed7c28e487d54a18b50935906e7
unknown <justusss@gmail.com>
2023-01-24 09:33:31 +0200

    your commit message here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from "axios";
+import { useQuery } from 'react-query'
 
 function App() {
+
+const fetchDataHandler = () => {
+  return axios
+    .get("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((res) => res.data.meals);
+};
+
+const {isError, isLoading, data, error} = useQuery(
+  'meal', 
+  fetchDataHandler
+  );
+
+  if(isLoading) {
+    return <p>Loading....</p>
+  }
+
+  if(isError) {
+    return <p>{error}</p>
+  }
+
+  console.log("data is ", data);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5>{data[0].strMeal}</h5>
+      <img src={data[0].strMealThumb} alt="#"/>
+        <button type="">Get Another Meal</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ // Create a client
+ const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
